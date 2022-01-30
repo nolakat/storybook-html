183d04d95bba10e9548af3be5542e88577210f87
nolakat <nolakat@users.noreply.github.com>
2022-01-30 21:59:37 +0000

    [Bot] Update dist

--- dist/stories/Button.js
+++ dist/stories/Button.js
@@ -1,21 +1,65 @@
 import './button.css';
 
 export const createButton = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
+  hollow = false,
+  size = 'small',
   label,
   onClick,
 }) => {
   const btn = document.createElement('button');
+  let btnPadding;
+
   btn.type = 'button';
   btn.innerText = label;
+  btn.ariaLabel = label;
+  btn.tabIndex = "0";
+  btn.ariaRoleDescription = "button"
+  btn.ariaPressed = "false"
   btn.addEventListener('click', onClick);
 
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  btn.className = ['storybook-button', `storybook-button--${size}`, mode].join(' ');
+  const isHollow = hollow ? 'bg-transparent' : 'bg-black';
+  const textColor =  'text-white';
+  const textWeight = 'font-medium';
+  const isBorder =  hollow ? 'border-2 border-white' : 'border-2 border-black';
+  const textSize = size == 'medium' ? 'text-base' : 'text-tiny';
+  const borderRadius = size == 'medium' ? 'rounded-md' : 'rounded-sm';
+  const onHover = hollow ?  'hover:bg-black hover:text-white hover:border-black' : 'hover:bg-white hover:text-black hover:border-white';
+  const btnTransition = 'ease-in duration-300';
 
-  btn.style.backgroundColor = backgroundColor;
 
+  switch(size){
+    case 'small':
+      btnPadding = 'py-3 px-7.5'
+    break;
+    case 'medium':
+      btnPadding = 'py-3 px-21.75'
+    break;
+  }
+  btn.className = `${isHollow} ${textWeight} ${isBorder} ${textSize} ${textColor} ${btnPadding} ${borderRadius} ${onHover} ${btnTransition}`;
   return btn;
 };
+
+export const createIcon = () =>{
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M5 12H19" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M12 5L19 12L12 19"stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`
+}
+
+
+export const createIconButton = ({
+  label,
+  onClick
+}) => {
+  const btn = document.createElement('button');
+  btn.innerHTML = `<span>${label}</span>${createIcon()}`;
+  btn.ariaLabel = label;
+  btn.tabIndex = "0";
+  btn.ariaRoleDescription = "button"
+  btn.ariaPressed = "false"
+  btn.addEventListener('click', onClick);
+
+  btn.className = `text-white py-3 px-4 hover:text-black border-2 border-black bg-black hover:bg-transparent rounded-md flex flex-row w-full justify-between hover--svg max-w-66 transition--default`
+
+  return btn;
+}
--- dist/stories/Button.stories.js
+++ dist/stories/Button.stories.js
@@ -1,48 +1,43 @@
-import { createButton } from './Button';
+import { createButton, createIconButton } from './Button';
 
 // More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 export default {
   title: 'Control/Button',
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
     label: { control: 'text' },
     onClick: { action: 'onClick' },
-    primary: { control: 'boolean' },
+    hollow: { control: 'boolean' },
     size: {
       control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
+      options: ['small', 'medium'],
     },
   },
 };
 
 // More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
-const Template = ({ label, ...args }) => {
+const PrimaryTemplate = ({ label, ...args }) => {
   // You can either use a function to create DOM elements or use a plain html string!
   // return `<div>${label}</div>`;
   return createButton({ label, ...args });
 };
 
-export const Primary = Template.bind({});
+const IconTemplate = ({ label, ...args }) =>{
+  console.log('TEST', args)
+  return createIconButton({ label, args });
+}
+
+export const Primary = PrimaryTemplate.bind({});
 // More on args: https://storybook.js.org/docs/html/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
+  hollow: true,
   size: 'small',
   label: 'Button',
 };
+
+export const Icon = IconTemplate.bind({});
+Icon.args = {
+  hollow: true,
+  label: 'Button',
+};
+
